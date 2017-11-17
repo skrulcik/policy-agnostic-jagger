a98b104c38b3b2ba30389e7d5d3ef9ac9a4afcca
Scott Krulcik <skrulcik@gmail.com>
2017-11-17 01:56:11 -0500

    Clarify the purpose of ProductionExecutorModule
    This class is meant for public use, so I added a Javadoc explaining that
    it should be used for all policy-agnostic dagger producers graphs.
    
    I also changed the executor to single-threaded because we aren't ready
    for concurrency.

--- agnostic/src/main/java/com/scottkrulcik/agnostic/ProductionExecutorModule.java
+++ agnostic/src/main/java/com/scottkrulcik/agnostic/ProductionExecutorModule.java
@@ -7,9 +7,18 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * All programs using Dagger Producers need an production executor module. This module should
+ * provides the executor for all faceted execution to occur on.
+ *
+ * At the moment, we don't have well-defined concurrency semantics for policy agnostic
+ * programming, so we only ever supply a single-threaded executor.
+ *
+ * TODO(skrulcik): Enforce that this is the only executor used for PAP.
+ */
 @Module
 public final class ProductionExecutorModule {
-    private static final ExecutorService exec = Executors.newCachedThreadPool();
+    private static final ExecutorService exec = Executors.newSingleThreadExecutor();
 
     @Provides
     @Production
