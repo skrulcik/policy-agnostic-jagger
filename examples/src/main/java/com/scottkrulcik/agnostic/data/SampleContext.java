e9d7eeccb4e683c4b175109c9f6abc7017fafeeb
Scott Krulcik <skrulcik@gmail.com>
2018-02-13 22:43:05 -0500

    Generate component builders for Jagger contexts
    This is superior for three reasons:
    
    * Annotation processor consistency (gradle would process Dagger first,
        leading to compilation errors, but IntelliJ worked fine)
    * Less repetitive human code
    * Automatically enforce @Raw in the Builder class

--- examples/src/main/java/com/scottkrulcik/agnostic/data/SampleContext.java
+++ examples/src/main/java/com/scottkrulcik/agnostic/data/SampleContext.java
@@ -1,20 +1,9 @@
 package com.scottkrulcik.agnostic.data;
 
 import com.scottkrulcik.agnostic.annotations.JaggerContext;
-import com.scottkrulcik.agnostic.annotations.Raw;
-import dagger.BindsInstance;
-import dagger.Component;
 
 
 @JaggerContext
-@Component(modules = Jagger_SampleContextSanitizerModule.class)
 public interface SampleContext {
     SampleData data();
-
-    @Component.Builder
-    interface Builder {
-        @BindsInstance
-        Builder rawData(@Raw SampleData data);
-        SampleContext build();
-    }
 }
--- processor/src/main/java/com/scottkrulcik/agnostic/processor/RestrictionProcessor.java
+++ processor/src/main/java/com/scottkrulcik/agnostic/processor/RestrictionProcessor.java
@@ -2,27 +2,35 @@ package com.scottkrulcik.agnostic.processor;
 
 import static com.scottkrulcik.agnostic.processor.AnnotationUtils.getAnnotationMirror;
 import static com.scottkrulcik.agnostic.processor.AnnotationUtils.getAnnotationValue;
-import static java.lang.Character.toUpperCase;
+import static javax.lang.model.element.ElementKind.METHOD;
 
 import com.google.auto.common.BasicAnnotationProcessor;
 import com.google.auto.service.AutoService;
+import com.google.common.base.Preconditions;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.SetMultimap;
 import com.scottkrulcik.agnostic.annotations.JaggerContext;
 import com.scottkrulcik.agnostic.annotations.Raw;
 import com.scottkrulcik.agnostic.annotations.Restrict;
+import com.squareup.javapoet.AnnotationSpec;
+import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterSpec;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
+import dagger.BindsInstance;
+import dagger.Component;
 import dagger.Provides;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.RoundEnvironment;
@@ -46,10 +54,11 @@ public class RestrictionProcessor extends BasicAnnotationProcessor {
     private static final String LABEL_FIELD = "label";
 
     /**
-     * Holds the most recent type mirror for the given canonical type string. I observed that
-     * {@link TypeMirror}s are not equal across runs, even using the
-     * {@link com.google.auto.common.MoreTypes MoreTypes} equivalence. This is the best way I can
-     * think of to keep track of type mirrors without duplicating.
+     * Holds the most recent type mirror for the given canonical type string. I observed that {@link
+     * TypeMirror}s are not equal across runs, even using the {@link com.google.auto.common
+     * .MoreTypes
+     * MoreTypes} equivalence. This is the best way I can think of to keep track of type mirrors
+     * without duplicating.
      */
     private final Map<String, TypeMirror> canonicalLabels = new HashMap<>();
     // TODO(skrulcik): Consider whether this should be maintained across runs
@@ -76,10 +85,57 @@ public class RestrictionProcessor extends BasicAnnotationProcessor {
         }
     }
 
-    private static String sanitizerName(Element originalElement) {
-        return "Jagger_" + originalElement.getSimpleName() + "SanitizerModule";
+    private static final EnumSet<ElementKind> CLASS_LIKE_ELEMENT_KINDS =
+        EnumSet.of(ElementKind.CLASS, ElementKind.INTERFACE);
+
+    private static String sanitizerName(Element originalClass) {
+        Preconditions.checkArgument(CLASS_LIKE_ELEMENT_KINDS.contains(originalClass.getKind()));
+        return originalClass.getSimpleName() + "SanitizerModule";
     }
 
+    private static String contextComponentName(Element originalClass) {
+        Preconditions.checkArgument(CLASS_LIKE_ELEMENT_KINDS.contains(originalClass.getKind()));
+        return originalClass.getSimpleName() + "Component";
+    }
+
+    private static String getSimpleName(TypeMirror typeMirror) {
+        String[] tokens = typeMirror.toString().split("\\.");
+        return tokens[tokens.length - 1];
+    }
+
+    private static String rawMethodName(Element originalMethod) {
+        Preconditions.checkArgument(originalMethod.getKind().equals(METHOD));
+        String lowerCamelName = originalMethod.getSimpleName().toString();
+        char firstCharacter = lowerCamelName.charAt(0);
+        String upperCamelName = Character.toUpperCase(firstCharacter) + lowerCamelName.substring(1);
+        return "raw" + upperCamelName;
+    }
+
+    /**
+     * Returns the instance methods defined within the given class.
+     */
+    private static List<ExecutableElement> instanceMethods(Element classElement) {
+        Preconditions.checkArgument(CLASS_LIKE_ELEMENT_KINDS.contains(classElement.getKind()));
+
+        return classElement.getEnclosedElements().stream()
+            .filter(e -> e.getKind().equals(METHOD))
+            .map(e -> (ExecutableElement) e)
+            .collect(Collectors.toList());
+    }
+
+    private void writeToFile(JavaFile outputFile) {
+        Filer filer = processingEnv.getFiler();
+        Messager messager = processingEnv.getMessager();
+        try {
+            messager.printMessage(Kind.NOTE,
+                "+ generating " + outputFile.typeSpec.name);
+            outputFile.writeTo(filer);
+        } catch(IOException e) {
+            e.printStackTrace();
+            messager.printMessage(Kind.NOTE,
+                "Error writing to java file " + outputFile.typeSpec.name);
+        }
+    }
     private final class CreateSanitizerModules implements ProcessingStep {
 
         @Override
@@ -87,67 +143,120 @@ public class RestrictionProcessor extends BasicAnnotationProcessor {
             return Collections.singleton(JaggerContext.class);
         }
 
+        private void createSanitizerModule(Filer filer, Messager messager, Element originalClass) {
+            messager.printMessage(Kind.NOTE, "Creating sanitizer for " + originalClass); // SCOTT DEBUG ONLY
+            TypeSpec.Builder sanitizerModule = TypeSpec.classBuilder(sanitizerName(originalClass))
+                .addAnnotation(dagger.Module.class)
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL);
+
+            originalClass.getEnclosedElements().stream().filter(e -> e
+                .getKind().equals(METHOD)).forEach(element ->
+                {
+                    ExecutableElement method = (ExecutableElement) element;
+
+                    // Create a provider method name of the form providesTypeName
+                    String methodName = "provides" + getSimpleName(method.getReturnType());
+
+                    // Determine the type of the method parameter (we'll add the Raw)
+                    TypeName paramType = TypeName.get(method.getReturnType());
+
+                    ParameterSpec providerParam = ParameterSpec.builder(paramType, "rawValue")
+                        .addAnnotation(Raw.class)
+                        .build();
+
+                    sanitizerModule.addMethod(MethodSpec.methodBuilder(methodName)
+                        .addAnnotation(Provides.class)
+                        .addParameter(providerParam)
+                        .addStatement("return $N.sanitize()", providerParam)
+                        .returns(paramType)
+                        .build());
+                }
+            );
+
+            // Write the faceted version of the class into a java source file
+            String originalPackage = getEnclosingPackage(originalClass).toString();
+            JavaFile outputFile = JavaFile.builder(originalPackage, sanitizerModule.build())
+                .build();
+            writeToFile(outputFile);
+        }
+
+        private void createComponent(Filer filer, Messager messager, Element originalClass) {
+            // Create a context component interface, and inner Builder interface
+            String packageName = getEnclosingPackage(originalClass).getQualifiedName().toString();
+            String contextComponentName = contextComponentName(originalClass);
+            ClassName contextBuilderName =
+                ClassName.get(packageName, contextComponentName, "Builder");
+
+            TypeSpec.Builder contextComponent =
+                TypeSpec.interfaceBuilder(contextComponentName)
+                .addSuperinterface(TypeName.get(originalClass.asType()));
+            TypeSpec.Builder contextBuilder = TypeSpec.interfaceBuilder(contextBuilderName)
+                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                .addAnnotation(Component.Builder.class);
+
+
+            AnnotationSpec componentAnnotation = AnnotationSpec.builder(Component.class)
+                .addMember("modules", sanitizerName(originalClass) + ".class")
+                .build();
+            contextComponent.addAnnotation(componentAnnotation);
+
+            for (ExecutableElement method : instanceMethods(originalClass)) {
+                TypeName originalReturnType = TypeName.get(method.getReturnType());
+                ParameterSpec rawParam =
+                    ParameterSpec.builder(originalReturnType, method.getSimpleName().toString())
+                    .addAnnotation(Raw.class)
+                    .build();
+
+                String rawMethodName = rawMethodName(method);
+                MethodSpec builderMethod = MethodSpec.methodBuilder(rawMethodName)
+                    .addAnnotation(BindsInstance.class)
+                    .addParameter(rawParam)
+                    .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
+                    .returns(contextBuilderName)
+                    .build();
+                contextBuilder.addMethod(builderMethod);
+            }
+
+            // add a build method for the inner Builder interface
+            MethodSpec buildMethod = MethodSpec.methodBuilder("build")
+                .addModifiers(Modifier.ABSTRACT, Modifier.PUBLIC)
+                .returns(TypeName.get(originalClass.asType()))
+                .build();
+            contextBuilder.addMethod(buildMethod);
+            // add the builder itself to the component
+            contextComponent.addType(contextBuilder.build());
+
+            JavaFile contextComponentFile =
+                JavaFile.builder(packageName, contextComponent.build()).build();
+            writeToFile(contextComponentFile);
+        }
+
         @Override
         public Set<? extends Element> process(SetMultimap<Class<? extends Annotation>, Element> elementsByAnnotation) {
-            System.out.println("Processing " + elementsByAnnotation);
             Filer filer = processingEnv.getFiler();
             Messager messager = processingEnv.getMessager();
+            messager.printMessage(Kind.NOTE, "Processing " + elementsByAnnotation);
 
             for (Element originalClass : elementsByAnnotation.get(JaggerContext.class)) {
-                System.out.println("Creating sanitizer for " + originalClass); // SCOTT DEBUG ONLY
-                TypeSpec.Builder sanitizerModule = TypeSpec.classBuilder(sanitizerName
-                    (originalClass))
-                    .addAnnotation(dagger.Module.class)
-                    .addModifiers(Modifier.PUBLIC, Modifier.FINAL);
-
-                originalClass.getEnclosedElements().stream().filter(e -> e
-                    .getKind().equals(ElementKind.METHOD)).forEach(element ->
-                    {
-                        ExecutableElement method = (ExecutableElement) element;
-
-                        // Create a provider method name
-                        String methodName = method.getSimpleName().toString();
-                        char c = methodName.charAt(0);
-                        methodName = "provides" + toUpperCase(c) + methodName.substring(1);
-
-                        // Determine the type of the method parameter (we'll add the Raw)
-                        // TODO(skrulcik): Validate that exactly one @Raw parameter exists
-                        TypeName paramType = TypeName.get(method.getReturnType());
-
-                        ParameterSpec providerParam = ParameterSpec.builder(paramType, "rawValue")
-                            .addAnnotation(Raw.class)
-                            .build();
-
-                        sanitizerModule.addMethod(MethodSpec.methodBuilder(methodName)
-                            .addAnnotation(Provides.class)
-                            .addParameter(providerParam)
-                            .addStatement("return $N.sanitize()", providerParam)
-                            .returns(paramType)
-                            .build());
-                    }
-                );
-
-                // Write the faceted version of the class into a java source file
-                String originalPackage = ((PackageElement) originalClass.getEnclosingElement())
-                    .getQualifiedName().toString();
-                JavaFile output = JavaFile.builder(originalPackage, sanitizerModule.build()).build();
-                try {
-                    System.out.println("Writing file to filer");
-                    output.writeTo(filer);
-                    System.out.println("Finished writing to " + originalPackage);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    messager.printMessage(Kind.ERROR, "Error writing to java file", originalClass);
-                }
+                createSanitizerModule(filer, messager, originalClass);
+                createComponent(filer, messager, originalClass);
             }
 
             return Collections.emptySet();
         }
     }
 
+    private static PackageElement getEnclosingPackage(Element originalClass) {
+        Preconditions.checkArgument(CLASS_LIKE_ELEMENT_KINDS.contains(originalClass.getKind()));
+        Element enclosingElement = originalClass.getEnclosingElement();
+        while (!enclosingElement.getKind().equals(ElementKind.PACKAGE)) {
+            enclosingElement = enclosingElement.getEnclosingElement();
+        }
+        return (PackageElement) enclosingElement;
+    }
+
     @Override
     protected Iterable<? extends ProcessingStep> initSteps() {
-        System.out.println("INIT PROCESSOR");
         return Collections.singletonList(new CreateSanitizerModules());
     }
 
@@ -155,8 +264,8 @@ public class RestrictionProcessor extends BasicAnnotationProcessor {
     protected void postRound(RoundEnvironment roundEnv) {
         super.postRound(roundEnv);
         if (roundEnv.processingOver()) {
-            System.out.println("Here are the labels: >>>>>>>>>>>>>>>>>>>>>>>>");
-            System.out.println(canonicalLabels);
+            processingEnv.getMessager().printMessage(Kind.NOTE,
+                "Canonical Labels: " + canonicalLabels);
         }
     }
 
