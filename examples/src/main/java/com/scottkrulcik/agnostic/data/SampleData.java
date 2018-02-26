dda2ab244f6254eccacfca5c034055f8aa14e323
Scott Krulcik <skrulcik@gmail.com>
2018-02-26 17:19:33 -0500

    [minor] re-order sample data members

--- examples/src/main/java/com/scottkrulcik/agnostic/data/SampleData.java
+++ examples/src/main/java/com/scottkrulcik/agnostic/data/SampleData.java
@@ -1,28 +1,50 @@
 package com.scottkrulcik.agnostic.data;
 
 import com.google.auto.value.AutoValue;
-import com.scottkrulcik.agnostic.LabelDefinition;
-import com.scottkrulcik.agnostic.ViewingContext;
-import com.scottkrulcik.agnostic.annotations.Faceted;
 import com.scottkrulcik.agnostic.annotations.Restrict;
 import com.scottkrulcik.agnostic.annotations.Restriction;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+
 import java.time.Instant;
-import java.util.Collections;
 import java.util.Date;
-import java.util.Set;
 import java.util.concurrent.Callable;
-import java.util.function.Predicate;
-import javax.inject.Qualifier;
 
 /**
- * Simple test class to test the {@link Faceted} annotation.
+ * Simple test class to test the {@link Restrict} and {@link Restriction} annotations.
  */
 @AutoValue
 public abstract class SampleData {
+
+    @Restrict(label = "creationDate", defaultValue = DefaultCreationDate.class)
+    public abstract Date creationDate();
+
+    @Restriction("creationDate")
+    boolean isCreationDateVisible() {
+        return false;
+    }
+
+    @Restrict(label = "name", defaultValue = DefaultName.class, dependencies = {"creationDate"})
+    public abstract String name();
+
+    @Restriction("name")
+    boolean isNameVisible() {
+        return true;
+    }
+
+
+    public abstract SampleData withName(String name);
+
+    public abstract SampleData withCreationDate(Date creationDate);
+
+    protected SampleData sanitize() {
+        SampleData sanitized = this;
+        if (!isNameVisible()) {
+            sanitized = this.withName(new DefaultName().call());
+        }
+        if (!isCreationDateVisible()) {
+            sanitized = this.withCreationDate(new DefaultCreationDate().call());
+        }
+        return sanitized;
+    }
 
     public static final class DefaultName implements Callable<String> {
 
@@ -38,73 +60,6 @@ public abstract class SampleData {
         public Date call() {
             return Date.from(Instant.EPOCH);
         }
-    }
-
-    public static final class AlwaysYesLabel extends LabelDefinition<ViewingContext> {
-
-        @Target(ElementType.PARAMETER)
-        @Retention(RetentionPolicy.RUNTIME)
-        @Qualifier
-        public @interface Result { }
-
-        @Override
-        public Set<Predicate<ViewingContext>> restrictions() {
-            return Collections.emptySet();
-        }
-
-        @Override
-        public Set<Class<? extends LabelDefinition<?>>> dependencies() {
-            return Collections.emptySet();
-        }
-    }
-
-
-    public static final class AlwaysNoLabel extends LabelDefinition<ViewingContext> {
-
-        @Target(ElementType.PARAMETER)
-        @Retention(RetentionPolicy.RUNTIME)
-        @Qualifier
-        public @interface Result { }
-
-        @Override
-        public Set<Predicate<ViewingContext>> restrictions() {
-            return Collections.singleton(vc -> false);
-        }
-
-        @Override
-        public Set<Class<? extends LabelDefinition<?>>> dependencies() {
-            return Collections.emptySet();
-        }
-    }
-
-    @Restrict(label = "name", defaultValue = DefaultName.class)
-    public abstract String name();
-
-    @Restrict(label = "creationDate", defaultValue = DefaultCreationDate.class)
-    public abstract Date creationDate();
-
-    public abstract SampleData withName(String name);
-    public abstract SampleData withCreationDate(Date creationDate);
-
-    protected SampleData sanitize() {
-        SampleData sanitized = this;
-        if (!isNameVisible()) {
-            sanitized = this.withName(new DefaultName().call());
-        }
-        if (!isCreationDateVisible()) {
-            sanitized = this.withCreationDate(new DefaultCreationDate().call());
-        }
-        return sanitized;
-    }
-
-    @Restriction("creationDate")
-    boolean isCreationDateVisible() {
-        return false;
-    }
-
-    @Restriction("name")
-    boolean isNameVisible() {
-        return true;
     }
 
 }
