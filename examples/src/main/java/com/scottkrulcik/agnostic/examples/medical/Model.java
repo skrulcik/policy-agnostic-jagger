824f54a080d16eb85d5505bca6509b8761406520
Scott Krulcik <skrulcik@gmail.com>
2018-04-10 14:26:23 -0400

    Annotate the medical model

--- examples/src/main/java/com/scottkrulcik/agnostic/examples/medical/Model.java
+++ examples/src/main/java/com/scottkrulcik/agnostic/examples/medical/Model.java
@@ -1,8 +1,25 @@
 package com.scottkrulcik.agnostic.examples.medical;
 
 import com.google.auto.value.AutoValue;
+import com.scottkrulcik.agnostic.annotations.Restrict;
+import com.scottkrulcik.agnostic.annotations.Restriction;
+import com.scottkrulcik.agnostic.annotations.SafeDefault;
+
+import javax.inject.Qualifier;
 
 final class Model {
+
+    @SafeDefault("psychNoteRule")
+    public static final String REDACTED = "REDACTED";
+
+    @SafeDefault("patient")
+    public static final Person ANON_PATIENT = Person.create("Anonymous");
+
+    @SafeDefault("provider")
+    public static final Person ANON_DOC = Person.create("Anonymous");
+
+    @SafeDefault("record")
+    public static final Record EMPTY_REC = Record.create(ANON_PATIENT, ANON_DOC, REDACTED, false);
 
     @AutoValue
     static abstract class Person {
@@ -19,20 +36,50 @@ final class Model {
 
         abstract Person provider();
 
+        @Restrict("psychNoteRule")
         abstract String condition();
 
         abstract boolean isPsychNote();
 
+        abstract Record withCondition(String condition);
+
+        Record sanitizeCondition() {
+            return withCondition(REDACTED);
+        }
+
         static Record create(Person patient, Person provider, String condition, boolean isPsychNote) {
             return new AutoValue_Model_Record(patient, provider, condition, isPsychNote);
         }
+
+        @Restriction("psychNoteRule")
+        public final boolean psychNoteRule(@Doctor Person requester, HardCoded.ConsentFormQuery.Builder query) {
+            HardCoded.ConsentFormQuery consentFormQuery =
+                query.matching(cf -> cf.provider().equals(requester) && cf.record().equals(this)).build();
+            return !this.isPsychNote()
+                || requester.equals(this.provider())
+                || !consentFormQuery.get().isEmpty();
+        }
     }
 
+
+    // TODO(skrulcik): allow patients to see it too
     @AutoValue
     static abstract class ConsentForm {
-        abstract Record record();
+        @Restrict("record")
+        public abstract Record record();
 
-        abstract Person provider();
+        public abstract Person provider();
+
+        abstract ConsentForm withRecord(Record record);
+
+        ConsentForm sanitizeRecord() {
+            return withRecord(EMPTY_REC);
+        }
+
+        @Restriction("record")
+        public final boolean formVisible(@Doctor Person doctor) {
+            return provider().equals(doctor);
+        }
 
         /**
          * Creates a consent form object stating that {@code patient} permits {@code provider} to
@@ -42,6 +89,12 @@ final class Model {
             return new AutoValue_Model_ConsentForm(record, provider);
         }
     }
+
+    @Qualifier
+    @interface Patient {}
+
+    @Qualifier
+    @interface Doctor {}
 
     private Model() {
     }
